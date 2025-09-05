2cbce1fd8b3589eec624bd4269af685e126fc539
Akilan247 <akilanb247@gmail.com>
2025-09-05 12:01:58 +0530

    Main.java file is deleted

--- Mini Database/ConsoleDatabase.java
+++ Mini Database/ConsoleDatabase.java
@@ -1,4 +1,3 @@
-
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
@@ -206,74 +205,6 @@ public class ConsoleDatabase {
                 System.out.print("+");
                 System.out.println();
                 System.out.println();
-            }
-        }catch (Exception e){
-            System.out.println("Error found");
-            System.out.println();
-        }
-    }
-}
-class Need {
-    String dbname;
-    public void operation() {
-        try {
-            Scanner scan = new Scanner(System.in);
-            ConsoleDatabase cd = new ConsoleDatabase();
-            String command;
-            int flag = 0;
-            while (true) {
-                command = scan.nextLine();
-                if (command.equalsIgnoreCase("exit")) {
-                    flag = 0;
-                    break;
-                } else if (command.startsWith("create database")) {
-                    cd.createDatabase(command);
-                    flag = 0;
-                } else if (command.startsWith("use")) {
-                    flag = 1;
-                    String name = cd.usedatabase(command);
-                    this.dbname = name;
-                    System.out.println("selected database is " + this.dbname);
-                    System.out.println();
-                } else if (command.startsWith("create table")) {
-                    if (flag == 1)
-                        cd.createTable(this.dbname, command);
-                    else {
-                        System.out.println("Database is not selected");
-                        System.out.println();
-                    }
-                } else if (command.startsWith("insert into")) {
-                    if (flag == 1)
-                        cd.insertIntoTable(this.dbname, command);
-                    else {
-                        System.out.println("Database is not selected");
-                        System.out.println();
-                    }
-                }else if (command.startsWith("delete from")) {
-                    if (flag == 1)
-                        cd.deleteFromTable(this.dbname, command);
-                    else {
-                        System.out.println("Database is not selected");
-                        System.out.println();
-                    }
-                } else if (command.startsWith("select * from")) {
-                    if (flag == 1)
-                        cd.selectFromTable(this.dbname, command);
-                    else {
-                        System.out.println("Database is not selected ");
-                        System.out.println();
-                    }
-                } else if (command.startsWith("desc")) {
-                    if (flag == 1)
-                        cd.tableStructure(this.dbname, command);
-                    else {
-                        System.out.println("Database is not selected ");
-                        System.out.println();
-                    }
-                } else {
-                    System.out.println("Invalid command. Try again.");
-                    System.out.println();
-                }
             }
         }catch (Exception e){
             System.out.println("Error found");
